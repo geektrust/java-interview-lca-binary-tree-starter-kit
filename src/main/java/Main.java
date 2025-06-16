780ba54d737a5a283fec672ba3dd3c5b2c4388c0
Himanshu Dubey <himanshu@geektrust.in>
2025-06-16 14:22:38 +0530

    removed solution from code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,11 +2,7 @@ package main.java;
 public class Main {
     /* Implement your solution inside this method. */
     public static TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if (root == null || root == p || root == q) return root;
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        if (left != null && right != null) return root;
-        return left != null ? left : right;
+        return null;
     }
     
      /*
